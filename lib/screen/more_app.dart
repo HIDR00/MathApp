061756d70c44514f5803d909d6d3c2084899679e
toantd <tdt271003@gmail.com>
2023-10-12 09:37:58 +0700

    Xong tat ca chuc nang

--- lib/screen/more_app.dart
+++ lib/screen/more_app.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../configs/style_configs.dart';
 import 'home/homepage.dart';
@@ -49,6 +51,60 @@ class _MoreAppState extends State<MoreApp> {
                   ),
                 ),
               ),
+          ),
+          body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image(image: AssetImage('assets/images/Group 3109.png')),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("English Vocab"),
+                            Wrap(
+                            direction: Axis.horizontal,
+                            children: List.generate(
+                                5,
+                                (index) => Icon(Icons.star, color: Color(0xFFeeab04)))),
+                            Row(
+                              children: [
+                                Image(image: AssetImage('assets/image/Download.png')),
+                                Text("1621")
+                              ],
+                            )
+                          ],
+                        ),
+                        GestureDetector(
+                          onTap: ()async {
+                  final likeUrl = Uri.parse('https://play.google.com/store/apps/details?id=vn.techlead.android.game.findword');
+                      if(await canLaunchUrl(likeUrl)){
+                        await launchUrl(likeUrl);
+                      }
+                },
+                          child: Container(
+                            height: 40,
+                            width: 60,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFFEB704),
+                              border: Border.all(color: stroke,width: 1),
+                              borderRadius: BorderRadius.circular(30)
+                            ),
+                            child: Center(child: Text("GET",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700),)),
+                          ),
+                        )
+                      ],
+                    ),
+                    Divider(color: Color(0xFFDCDCDC),thickness: 1,)
+                  ],
+                ),
+              );
+            },
           ),
         )
       ],
